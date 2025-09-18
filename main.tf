8654a41fb146883704463aec74a1d57a4eea91fd
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-18 13:00:52 +0100

    Removed Provider

--- main.tf
+++ main.tf
@@ -9,11 +9,6 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
-
 # Create a resource group
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
