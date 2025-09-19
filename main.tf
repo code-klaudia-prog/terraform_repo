64384480bc84f823db64dcfcdb60c3178a040379
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-19 15:55:07 +0100

    Added the Service Plan

--- main.tf
+++ main.tf
@@ -26,3 +26,11 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_service_plan" "example" {
+  name                = "svplan"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Windows"
+  sku_name            = "Y1"
+}
+
