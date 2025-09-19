5f28b5378375f3999a010cbf8f4836dc6d21174f
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-19 12:31:18 +0100

    Deploy without a Service Plan

--- main.tf
+++ main.tf
@@ -18,10 +18,32 @@ resource "azurerm_resource_group" "example" {
   location  = "West Europe"
 }
 
+# Service Plan Resource is missing for now. Let's test if we can deloy the reources below without a Service Plan
+
 resource "azurerm_storage_account" "example" {
-  name                     = "testofchangingnametofunctionappsa"
+  name                     = "claudia-test-storage-account"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "claudia-test-app-service-plan"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Windows"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_windows_function_app" "example" {
+  name                = "claudia-test-function-app"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {}
 }
