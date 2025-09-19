f120dfcf9c66610483cea35e683f5c12c8c2d8fd
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-19 12:29:30 +0100

    Changed the name attribute of the storage account

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "windowsfunctionappsa"
+  name                     = "testofchangingnametofunctionappsa"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
