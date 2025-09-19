f3ff757c47657f3239a6c79c09ab5e741e1278b9
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-19 09:41:02 +0100

    Network, App services and Database Deploy

--- main.tf
+++ main.tf
@@ -18,10 +18,37 @@ resource "azurerm_resource_group" "example" {
   location  = "West Europe"
 }
 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
+# --- Módulo de Rede ---
+module "network" {
+  source              = "./modules/network"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  vnet_name           = var.vnet_name
+  vnet_address_space  = var.vnet_address_space
+  frontend_subnet_name = "frontend-subnet"
+  backend_subnet_name  = "backend-subnet"
+  database_subnet_name = "database-subnet"
+}
+ 
+# --- Módulo de Banco de Dados ---
+module "database" {
+  source              = "./modules/database"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  vnet_id             = module.network.vnet_id
+  database_subnet_id  = module.network.database_subnet_id
+  postgres_server_name = var.postgres_server_name
+  postgres_db_name     = var.postgres_db_name
+}
+ 
+# --- Módulo de App Services e Application Gateway ---
+module "app_services" {
+  source              = "./modules/app_services"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  vnet_id             = module.network.vnet_id
+  frontend_subnet_id  = module.network.frontend_subnet_id
+  backend_subnet_id   = module.network.backend_subnet_id
+  app_gateway_subnet_id = module.network.app_gateway_subnet_id
+  database_server_name  = module.database.postgres_server_name
 }
