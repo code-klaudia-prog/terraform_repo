a730f51190bbd33d02ff9697a63b7d7198ced2a9
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-19 15:19:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,3 +25,12 @@ resource "azurerm_service_plan" "example" {
   os_type             = "Windows"
   sku_name            = "Y1"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storeacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
