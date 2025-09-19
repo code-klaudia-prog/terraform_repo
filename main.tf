fadaea8366afa91c49d8aadbb3ee9e6e09821743
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-19 15:35:03 +0100

    Storage Account

--- main.tf
+++ main.tf
@@ -18,16 +18,8 @@ resource "azurerm_resource_group" "example" {
   location  = "West Europe"
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "servplanklau"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  os_type             = "Windows"
-  sku_name            = "Y1"
-}
-
 resource "azurerm_storage_account" "example" {
-  name                     = "storeacc"
+  name                     = "stoact"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
