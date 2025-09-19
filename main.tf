c977721d29d99b3ed12841fd9737d4abd8bff6a3
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-19 09:43:12 +0100

    Replacement of variables by hardcoded values

--- main.tf
+++ main.tf
@@ -23,32 +23,32 @@ module "network" {
   source              = "./modules/network"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  vnet_name           = var.vnet_name
-  vnet_address_space  = var.vnet_address_space
+  vnet_name           = "meu-vnet-producao"
+  vnet_address_space  = ["10.0.0.0/16"]
   frontend_subnet_name = "frontend-subnet"
   backend_subnet_name  = "backend-subnet"
   database_subnet_name = "database-subnet"
 }
- 
+
 # --- Módulo de Banco de Dados ---
 module "database" {
-  source              = "./modules/database"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  vnet_id             = module.network.vnet_id
-  database_subnet_id  = module.network.database_subnet_id
-  postgres_server_name = var.postgres_server_name
-  postgres_db_name     = var.postgres_db_name
+  source               = "./modules/database"
+  resource_group_name  = azurerm_resource_group.main.name
+  location             = azurerm_resource_group.main.location
+  vnet_id              = module.network.vnet_id
+  database_subnet_id   = module.network.database_subnet_id
+  postgres_server_name = "meu-db-server"
+  postgres_db_name     = "meu-db-app"
 }
- 
+
 # --- Módulo de App Services e Application Gateway ---
 module "app_services" {
-  source              = "./modules/app_services"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  vnet_id             = module.network.vnet_id
-  frontend_subnet_id  = module.network.frontend_subnet_id
-  backend_subnet_id   = module.network.backend_subnet_id
+  source                = "./modules/app_services"
+  resource_group_name   = azurerm_resource_group.main.name
+  location              = azurerm_resource_group.main.location
+  vnet_id               = module.network.vnet_id
+  frontend_subnet_id    = module.network.frontend_subnet_id
+  backend_subnet_id     = module.network.backend_subnet_id
   app_gateway_subnet_id = module.network.app_gateway_subnet_id
   database_server_name  = module.database.postgres_server_name
 }
