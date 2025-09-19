39d547c093a2b83a3b0797378180e443a584a161
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-19 14:53:33 +0100

    Attempt to Deploy a Service Plan and an Azure Function

--- main.tf
+++ main.tf
@@ -27,3 +27,23 @@ resource "azurerm_storage_account" "example" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_service_plan" "example" {
+  name                = "clauservplan"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Windows"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_windows_function_app" "example" {
+  name                = "claufuncapp"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {}
+}
