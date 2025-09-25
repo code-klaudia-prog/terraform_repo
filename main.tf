78a5cdd6d651730d7e0062f956c310d5fe40b2fd
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-25 20:38:50 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.7.0"
+    }
+  }
 }
+
 
 resource "awscc_elasticbeanstalk_application" "example" {
   application_name = "example"
