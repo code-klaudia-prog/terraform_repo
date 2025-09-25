96d7952955c01f09f8a45b3157b729c31471ce58
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-25 14:23:37 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "my_site" {
-  # Available inputs: https://github.com/futurice/terraform-utils/tree/master/aws_static_site#inputs
-  # Check for updates: https://github.com/futurice/terraform-utils/compare/v11.0...master
-  source = "git::ssh://git@github.com/futurice/terraform-utils.git//aws_static_site?ref=v11.0"
-
-  site_domain = "hello.example.com"
-}
-
 resource "aws_s3_bucket_object" "my_index" {
   bucket       = "${module.my_site.bucket_name}"
   key          = "index.html"
