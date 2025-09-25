6473a821f808a1f5c31e69c56526d72555ca6578
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-25 15:30:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
+# Define o provedor AWS
 provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
+  region = "us-east-1"  # Região onde os recursos serão criados
 }
 
-resource "aws_s3_bucket_object" "my_index" {
-  bucket       = "blabla"
-  key          = "index.html"
-  content      = "<pre>Hello World!</pre>"
-  content_type = "text/html; charset=utf-8"
+# Cria uma aplicação Elastic Beanstalk
+resource "aws_elastic_beanstalk_application" "my_application" {
+  name        = "claudiaapp"
+  description = "Aplicação de exemplo criada com Terraform"
+}
+
+# Cria um ambiente Elastic Beanstalk para a aplicação
+resource "aws_elastic_beanstalk_environment" "my_environment" {
+  name                = "envclaudia"
+  application         = aws_elastic_beanstalk_application.my_application.name
+  solution_stack_name = "Amazon Linux running Tomcat 6"
 }
