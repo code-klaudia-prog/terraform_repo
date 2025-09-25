6f6f62fc965dbe85f554a0e15ea6aa9015c09129
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-25 10:39:50 +0100

    Solution Stack has been modified

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_elastic_beanstalk_application" "my_application" {
 resource "aws_elastic_beanstalk_environment" "my_environment" {
   name                = "my-terraform-env"
   application         = aws_elastic_beanstalk_application.my_application.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.4.15 running Python 3.8" # Exemplo de stack. Você pode alterá-lo.
+  solution_stack_name = "64bit Amazon Linux 2 v3.8.3 running Python 3.11" # Exemplo de stack. Você pode alterá-lo.
 
   # Configurações do ambiente
   setting {
