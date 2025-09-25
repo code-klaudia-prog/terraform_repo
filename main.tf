bdb9978ebf834989e201da75875ff3570389a993
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-25 14:06:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
 
 # Define o provedor AWS
 provider "aws" {
-  region = "us-east-1"  # Região onde os recursos serão criados
+  region = "us-east-1"
 }
 
-resource "aws_elastic_beanstalk_environment" "environment" {
-  name                = "php-hello-world-environment"
-  application         = aws_elastic_beanstalk_application.application.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.1.2 running PHP 7.3"
+resource "aws_elastic_beanstalk_application" "tftest" {
+  name        = "tf-test-name"
+  description = "tf-test-desc"
+}
+
+resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+  name                = "tf-test-name"
+  application         = aws_elastic_beanstalk_application.tftest.name
+  solution_stack_name = "64bit Amazon Linux 2015.03 v2.0.3 running Go 1.4"
 }
