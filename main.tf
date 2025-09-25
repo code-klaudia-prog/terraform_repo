f3285625b5f36d1338c3f874d2ed78f2a4d64ba2
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-25 14:02:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,32 +4,8 @@ provider "aws" {
   region = "us-east-1"  # Região onde os recursos serão criados
 }
 
-# Cria uma aplicação Elastic Beanstalk
-resource "aws_elastic_beanstalk_application" "my_application" {
-  name        = "claudiaapp"
-  description = "Aplicação de exemplo criada com Terraform"
-}
-
-resource "aws_elastic_beanstalk_environment" "docker-env" {
-  name                = "Docker-env3"
-  application         = aws_elastic_beanstalk_application.my_application.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.4.1 running PHP 8.0"
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "InstanceType"
-    value     = "t2.micro" # Exemplo de tipo de instância
-  }
-
-  setting {
-    namespace = "aws:autoscaling:asg"
-    name      = "MinSize"
-    value     = "1"
-  }
-
-  setting {
-    namespace = "aws:autoscaling:asg"
-    name      = "MaxSize"
-    value     = "1"
-  }
+resource "aws_elastic_beanstalk_environment" "environment" {
+  name                = "php-hello-world-environment"
+  application         = aws_elastic_beanstalk_application.application.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.1.2 running PHP 7.3"
 }
