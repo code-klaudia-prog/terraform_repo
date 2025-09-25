74d1ff90369f208d8491dbef124860f831622724
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-25 20:23:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,83 +1,39 @@
-terraform {
-  required_providers {
-    aws = ">= 3.35.0"
-  }
+resource "awscc_elasticbeanstalk_application" "example" {
+  application_name = "example"
+  description      = "example"
 }
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-resource "aws_elastic_beanstalk_application" "eb_appl" {
-  name        = var.eb_app_name
-  description = "Claudia has built this application"
-}
-
-resource "aws_default_vpc" "default" {
-  tags = {
-    Name = "Default VPC"
-  }
-}
-
-data "aws_subnets" "default_subs" {
-  filter {
-    name   = "vpc-id"
-    values = [aws_default_vpc.default.id]
-  }
-}
-
-resource "aws_elastic_beanstalk_configuration_template" "tf_template" {
-  name                = "tf-test-template-config"
-  application         = aws_elastic_beanstalk_application.eb_appl.name
-  solution_stack_name = null
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "VPCId"
-    value     = aws_default_vpc.default.id
-  }
-
-  setting {
-    namespace = "aws:ec2:vpc"
-    name      = "Subnets"
-    value     = join(",", data.aws_subnets.default_subs.ids)
-  }
-
-  setting {
-      namespace = "aws:ec2:instances"
-      name = "InstanceTypes"
-      value = "t4g.micro"
-  }
-
-  setting {
-      namespace = "aws:ec2:instances"
-      name = "SupportedArchitectures"
-      value = "arm64"
-  }
-
-  setting {
-      namespace = "aws:autoscaling:asg"
-      name = "MinSize"
-      value = 1
-  }
-
-  setting {
-      namespace = "aws:autoscaling:asg"
-      name = "MaxSize"
-      value = 2
-  }
-
-  setting {
-      namespace = "aws:elasticbeanstalk:environment"
-      name = "EnvironmentType"
-      value = "LoadBalanced"
-  }
-}
-
-resource "aws_elastic_beanstalk_environment" "tfenvtest" {
-  name                = var.eb_env_name
-  application         = aws_elastic_beanstalk_application.eb_appl.name
-  template_name = aws_elastic_beanstalk_configuration_template.tf_template.name
-  version_label = var.app_version
-  
+resource "awscc_elasticbeanstalk_configuration_template" "example" {
+  application_name    = awscc_elasticbeanstalk_application.example.application_name
+  description         = "My sample configuration template"
+  solution_stack_name = "64bit Amazon Linux 2023 v4.1.2 running Python 3.11"
+  option_settings = [{
+    namespace   = "aws:autoscaling:launchconfiguration"
+    option_name = "IamInstanceProfile"
+    value       = "testRoleEC2"
+    },
+    {
+      namespace   = "aws:autoscaling:launchconfiguration"
+      option_name = "InstanceType"
+      value       = "t3.large"
+    },
+    {
+      namespace   = "aws:elasticbeanstalk:command"
+      option_name = "DeploymentPolicy"
+      value       = "Immutable"
+    },
+    {
+      namespace   = "aws:elasticbeanstalk:environment"
+      option_name = "LoadBalancerType"
+      value       = "application"
+    },
+    {
+      namespace   = "aws:elbv2:loadbalancer"
+      option_name = "AccessLogsS3Enabled"
+      value       = "false"
+    },
+    {
+      namespace   = "aws:elasticbeanstalk:environment"
+      option_name = "LoadBalancerIsShared"
+      value       = "false"
+  }]
 }
