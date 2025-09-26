193f193b457390037f4aad22f70ea056cfb7e00a
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-26 10:11:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -343,20 +343,3 @@ resource "aws_elastic_beanstalk_environment" "beanstalkappenv" {
   }
 
 }
-
-
-resource "aws_lb_listener" "https_redirect" {
-  load_balancer_arn = aws_elastic_beanstalk_environment.beanstalkappenv.load_balancers[0]
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
