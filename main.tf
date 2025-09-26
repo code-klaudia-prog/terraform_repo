489920cc2b9293d76ab7b1a989ff5c6cf6b5be55
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-26 05:30:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,10 +18,21 @@ resource "awscc_elasticbeanstalk_application" "example" {
   description      = "example"
 }
 
+
+locals {
+  solution_stacks = [
+    "64bit Amazon Linux 2023 v4.1.2 running Python 3.11",
+    "64bit Amazon Linux 2023 v4.1.2 running Python 3.9",
+    "64bit Amazon Linux 2023 v4.1.2 running Node.js 18",
+  ]
+}
+
 resource "awscc_elasticbeanstalk_configuration_template" "example" {
+  for_each = toset(local.solution_stacks)
+
   application_name    = awscc_elasticbeanstalk_application.example.application_name
-  description         = "My sample configuration template"
-  solution_stack_name = "64bit Amazon Linux 2023 v4.1.2 running Python 3.11"
+  description         = "My sample configuration template for ${each.value}"
+  solution_stack_name = each.value
   option_settings = [{
     namespace   = "aws:autoscaling:launchconfiguration"
     option_name = "IamInstanceProfile"
@@ -51,5 +62,5 @@ resource "awscc_elasticbeanstalk_configuration_template" "example" {
       namespace   = "aws:elasticbeanstalk:environment"
       option_name = "LoadBalancerIsShared"
       value       = "false"
-  }]
+    }]
 }
