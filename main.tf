8c6d32f0803b7aa5bf1530f4e810f8b902232a5d
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-26 06:05:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+# Summary: Creates a simple Network Load Balancer and an ASG
+
+# Documentation: https://www.terraform.io/docs/language/settings/index.html
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.38"
+    }
+  }
+}
+
+# Documentation: https://www.terraform.io/docs/language/providers/requirements.html
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "awscc_elasticbeanstalk_application" "example" {
   application_name = "example"
   description      = "example"
