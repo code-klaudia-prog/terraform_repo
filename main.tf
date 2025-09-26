49a8cd9f0b19c25e75fe1fdc0ea8ed4bfca766a8
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-26 06:55:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Summary: Creates a simple Network Load Balancer and an ASG
+# Summary: Create a simple AWS RDS DB Instance with MySQL
 
 # Documentation: https://www.terraform.io/docs/language/settings/index.html
 terraform {
@@ -14,56 +14,34 @@ terraform {
 # Documentation: https://www.terraform.io/docs/language/providers/requirements.html
 provider "aws" {
   region = "us-east-1"
+  default_tags {
+    tags = {
+      cs_terraform_examples = "aws_db_instance/simple"
+    }
+  }
 }
 
-resource "awscc_elasticbeanstalk_application" "example" {
-  application_name = "example"
-  description      = "example"
+# Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_instance
+# Explanation: This resource creates the DB Instance. This Example creates a MySql Server version 5.7 as a Managed RDS Service.
+# Allocates a MySQl version 5.7 in a t3.micro instance with a database name 'changeme_simple_aws_db_instance', username 'changeme_username' and passwd 'changeme_password'
+resource "aws_db_instance" "changeme_simple_aws_db_instance" {
+  allocated_storage   = 5
+  engine              = "mysql"
+  engine_version      = "5.7"
+  instance_class      = "db.t3.micro"
+  name                = "changeme_simple_aws_db_instance"
+  username            = "changemeusername"
+  password            = "changeme_password"
+  skip_final_snapshot = true
 }
 
-
-locals {
-  solution_stacks = [
-    "64bit Amazon Linux 2023 v4.1.2 running Python 3.11",
-    "64bit Amazon Linux 2023 v4.1.2 running Python 3.9",
-    "64bit Amazon Linux 2023 v4.1.2 running Node.js 18",
-  ]
-}
-
-resource "awscc_elasticbeanstalk_configuration_template" "example" {
-  for_each = toset(local.solution_stacks)
-
-  application_name    = awscc_elasticbeanstalk_application.example.application_name
-  description         = "My sample configuration template for ${each.value}"
-  solution_stack_name = each.value
-  option_settings = [{
-    namespace   = "aws:autoscaling:launchconfiguration"
-    option_name = "IamInstanceProfile"
-    value       = "testRoleEC2"
-    },
-    {
-      namespace   = "aws:autoscaling:launchconfiguration"
-      option_name = "InstanceType"
-      value       = "t3.large"
-    },
-    {
-      namespace   = "aws:elasticbeanstalk:command"
-      option_name = "DeploymentPolicy"
-      value       = "Immutable"
-    },
-    {
-      namespace   = "aws:elasticbeanstalk:environment"
-      option_name = "LoadBalancerType"
-      value       = "application"
-    },
-    {
-      namespace   = "aws:elbv2:loadbalancer"
-      option_name = "AccessLogsS3Enabled"
-      value       = "false"
-    },
-    {
-      namespace   = "aws:elasticbeanstalk:environment"
-      option_name = "LoadBalancerIsShared"
-      value       = "false"
-    }]
-}
+##Explanation:
+# Details on Arguments:
+# Required Argument -  allocated_storage: How Much storage will be allocated for the Database in Gbi
+# Required Argument -             engine: Engines Types like MySql, Postgres, MariaDB....
+# Required Argument -     engine_version: The above choosen version of Engine. For ex:  MySQL version 5.7
+# Required Argument -     instance_class: The size of the Instance (VM)
+# Required Argument -           username: The Master DB-User username
+# Required Argument -            passord: The master DB-User password
+# Optional Argument -               name: The Name of the Instance
+# Optional Argument -  skip_final_snapshot: It's needed for destroying the instance with Terraform Destroy
