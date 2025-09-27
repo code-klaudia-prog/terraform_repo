79a7c59a31cb5680a8fc116471043c2ab7027e78
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-27 12:22:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ provider "aws" {
 data "aws_elastic_beanstalk_hosted_zone" "current" {}
 
 
-resource "awsc_elasticbeanstalk_application" "example" {
+resource "aws_elasticbeanstalk_application" "example" {
   application_name = "Example-App"
   description      = "Example-App"
 }
