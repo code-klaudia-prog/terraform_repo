bf4a47f02d3d996b65de216482e7e3f37867498e
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-29 09:54:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,6 +42,43 @@ resource "aws_subnet" "public_subnet_b" {
   map_public_ip_on_launch = true
 }
 
+
+# Policy de Confiança para permitir que as instâncias EC2 assumam a função
+data "aws_iam_policy_document" "eb_ec2_assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# Criação do EC2 Role
+resource "aws_iam_role" "eb_ec2_role" {
+  name               = "aws-elasticbeanstalk-ec2-role"
+  assume_role_policy = data.aws_iam_policy_document.eb_ec2_assume_role.json
+}
+
+# Anexa a política gerida da AWS para o perfil de instância do Elastic Beanstalk
+resource "aws_iam_role_policy_attachment" "eb_ec2_policy_attach_managed" {
+  role       = aws_iam_role.eb_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+}
+
+# **OPCIONAL mas RECOMENDADO:** Anexar uma segunda política (se necessário para *worker environments* ou logs)
+resource "aws_iam_role_policy_attachment" "eb_ec2_policy_attach_worker" {
+  role       = aws_iam_role.eb_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier"
+}
+
+# Criação do IAM Instance Profile que as instâncias EC2 irão usar
+resource "aws_iam_instance_profile" "eb_instance_profile" {
+  name = "aws-elasticbeanstalk-ec2-role"
+  role = aws_iam_role.eb_ec2_role.name
+}
+
 resource "aws_elastic_beanstalk_environment" "beanstalkappenv" {
   name                ="${var.app_tags}-Api"
   application         = aws_elastic_beanstalk_application.example.name
