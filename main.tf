056b46f0df9bae03185dd1a87d0a8b95957c5530
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-30 17:02:01 +0100

    Added a VPC and 2 Subnets

--- main.tf
+++ main.tf
@@ -16,6 +16,26 @@ terraform {
   }
 }
 
+resource "aws_vpc" "minha_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+ 
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.minha_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true  # Permite que as instâncias recebam IPs públicos
+}
+ 
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.minha_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b" # Altere para a sua região e AZ
+  map_public_ip_on_launch = true
+}
+
 #### Create the IAM role for the instance profile ####
 
 resource "aws_iam_role" "ssm_role" {
