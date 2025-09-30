fe7dfe2f81986e213da9e0a7f3035d9f22970f92
code-klaudia-prog <mywhoosh89@gmail.com>
2025-09-30 14:37:38 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,12 +11,6 @@ variable "private_subnet"{
   default     = false
 }
 
-variable "subnet_id" {
-  type        = string
-  description = "ID of subnet to deploy the instance in."
-  default     = ""
-}
-
 # variable "tags" {    
 #   description = "Common tags that should be used on specific resources"
 #   type        = map(string)
@@ -82,6 +76,7 @@ variable "s3_log_bucket_id" {
   description = "Name of the S3 logging bucket to deliver S3 server logs to. BUCKET MUST BE EXISTING!"
   default     = ""
 }
+
 
 
 
