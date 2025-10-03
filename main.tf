d5436ce92d8ef64507ad1f0890b6094f72c36f60
code-klaudia-prog <mywhoosh89@gmail.com>
2025-10-03 15:23:15 +0100

    Reordered Resources

--- main.tf
+++ main.tf
@@ -16,37 +16,12 @@ terraform {
   }
 }
 
-# 1. Cria o Internet Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.minha_vpc.id
-}
-
-# 2. Cria a Tabela de Rotas para Internet
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.minha_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-}
-
-# 3. Associa as Subnets à Tabela de Rotas
-resource "aws_route_table_association" "public_assoc_1" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "public_assoc_2" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.public_rt.id
-}
 resource "aws_vpc" "minha_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
- 
+
 resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.minha_vpc.id
   cidr_block        = "10.0.3.0/24"
@@ -61,7 +36,55 @@ resource "aws_subnet" "public_subnet_2" {
   map_public_ip_on_launch = true
 }
 
-#### Create the IAM role for the instance profile ####
+resource "aws_security_group" "http_allow" {
+  name        = var.security_group
+  description = "Security group to allow traffic over HTTPS 443"
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow outbound traffic over 443"
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.private_subnet ? ["${data.aws_subnet.ec2_subnet.cidr_block}"  ] : ["0.0.0.0/0"]
+    description = "allow outbound traffic over 443"
+  }
+
+  vpc_id = data.aws_vpc.desired_vpc.id
+
+}
+
+resource "aws_instance" "ssm_instance" {
+  ami                    = "ami-052064a798f08f0d3"
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.http_allow.id]
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+  monitoring             = true
+  subnet_id              = aws_subnet.public_subnet_1.id
+  associate_public_ip_address = var.private_subnet ? false : true
+
+  root_block_device {
+    encrypted  = true
+  }
+
+  metadata_options {
+    http_tokens                 = "required"
+    http_endpoint               = "enabled"
+    http_put_response_hop_limit = 1
+    instance_metadata_tags      = "disabled"
+  }
+}
+
+#### Create the instance profile to attach to the instance ####
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "ssm-ec2-profile-${var.team}"
+  role = aws_iam_role.ssm_role.name
+}
 
 resource "aws_iam_role" "ssm_role" {
   name = "${var.ssm_role}-${var.team}"
@@ -84,6 +107,34 @@ resource "aws_iam_role" "ssm_role" {
     ssmdemo = "true"
   }
 }
+
+# Cria o Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.minha_vpc.id
+}
+
+# Cria a Tabela de Rotas para Internet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.minha_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+# Associa as Subnets à Tabela de Rotas
+resource "aws_route_table_association" "public_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+#### Create the IAM role for the instance profile ####
 
 #### Create Policy to allow instance profile to put objects in the S3 bucket ####
 
@@ -169,34 +220,6 @@ data "aws_route_table" "subnet_rt" {
     name   = "association.main"
     values = ["true"]
   }
-}
-
-resource "aws_instance" "ssm_instance" {
-  ami                    = "ami-052064a798f08f0d3"
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.http_allow.id]
-  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
-  monitoring             = true
-  subnet_id              = aws_subnet.public_subnet_1.id
-  associate_public_ip_address = var.private_subnet ? false : true
-
-  root_block_device {
-    encrypted  = true
-  }
-
-  metadata_options {
-    http_tokens                 = "required"
-    http_endpoint               = "enabled"
-    http_put_response_hop_limit = 1
-    instance_metadata_tags      = "disabled"
-  }
-}
-
-#### Create the instance profile to attach to the instance ####
-
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "ssm-ec2-profile-${var.team}"
-  role = aws_iam_role.ssm_role.name
 }
 
 #### Create the security group to allow EC@ outbound traffic over 443 to interact with session manager ####
