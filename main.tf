26e14f991a839c5e0e1452c8a2ae8b8eaec204f6
code-klaudia-prog <mywhoosh89@gmail.com>
2025-10-07 14:58:21 +0100

    Attempt to deploy an Ubuntu EC2 instance
    https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance

--- main.tf
+++ main.tf
@@ -16,87 +16,27 @@ terraform {
   }
 }
 
-resource "aws_vpc" "vpc_minha" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-}
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.vpc_minha.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true  # Permite que as instâncias recebam IPs públicos
-}
-
-resource "aws_security_group" "http_allow" {
-  name        = var.security_group
-  description = "Security group to allow traffic over HTTPS 443"
-  egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "allow outbound traffic over 443"
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = var.private_subnet ? ["${ aws_subnet.public_subnet_1.cidr_block}"  ] : ["0.0.0.0/0"]
-    description = "allow outbound traffic over 443"
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
-  vpc_id = aws_vpc.vpc_minha.id
-
-}
-
-resource "aws_instance" "ssm_instance" {
-  ami                    = "ami-052064a798f08f0d3"
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.http_allow.id]
-  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
-  monitoring             = true
-  subnet_id              = aws_subnet.public_subnet_1.id
-  associate_public_ip_address = var.private_subnet ? false : true
-
-  root_block_device {
-    encrypted  = true
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 
-  metadata_options {
-    http_tokens                 = "required"
-    http_endpoint               = "enabled"
-    http_put_response_hop_limit = 1
-    instance_metadata_tags      = "disabled"
-  }
+  owners = ["099720109477"] # Canonical
 }
 
-#### Create the instance profile to attach to the instance ####
-
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "ssm-ec2-profile-${var.team}"
-  role = aws_iam_role.ssm_role.name
-}
-
-resource "aws_iam_role" "ssm_role" {
-  name = "${var.ssm_role}-${var.team}"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "EC2AssumeRole"
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
   tags = {
-    ssmdemo = "true"
+    Name = "HelloWorld"
   }
 }
