1f34d6147886e91e8774f46dca4884bf74df655a
Code Klaudia <mywhoosh89@gmail.com>
2025-10-09 00:18:04 +0100

    associate_public_ip_address = true

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_vpc" "my_custom_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.my_custom_vpc.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true # This allows the EC2 instance to get a public IP
+  associate_public_ip_address = true
   availability_zone       = "us-east-1a"
   tags = {
     Name = "ssm"
